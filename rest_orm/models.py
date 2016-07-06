e9334815e25647a05fcd83b775fb5ebc3a5bab41
Colton Allen <cmanallen90@gmail.com>
2016-07-06 09:47:19 -0400

    Restructured model to return a seperate datatype

--- rest_orm/models.py
+++ rest_orm/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from rest_orm.fields import AdaptedField
+from rest_orm.fields import Field
 from rest_orm.utils import ModelRegistry
 
 import json
@@ -11,34 +11,33 @@ class BaseModel(object):
     __metaclass__ = ModelRegistry
 
 
-class AdaptedModel(BaseModel):
+class Model(BaseModel):
     """A flat representation of a single remote endpoint."""
+
+    def loads(self, data):
+        """Load a JSON string to a flattened dictionary."""
+        return self.load(json.loads(data))
+
+    def load(self, data):
+        """Flatten a nested dictionary."""
+        response = {}
+        for field_name in dir(self):
+            field = getattr(self, field_name)
+            if not isinstance(field, Field):
+                continue
+            response[field_name] = field.deserialize(data)
+
+        response = self.post_load(response)
+        return response
 
     def connect(self, *args, **kwargs):
         """Make a request to a remote endpoint and load its JSON response."""
         response = self.make_request(*args, **kwargs)
         return self.loads(response)
 
-    def loads(self, response):
-        """Marshal a JSON response object into the model."""
-        return self.load(json.loads(response))
-
-    def load(self, response):
-        """Marshal a python dictionary object into the model."""
-        self._do_load(response)
-        self.post_load()
-        return self
-
-    def post_load(self):
+    def post_load(self, data):
         """Perform any model level actions after load."""
-        pass
-
-    def _do_load(self, data):
-        for field_name in dir(self):
-            field = getattr(self, field_name)
-            if not isinstance(field, AdaptedField):
-                continue
-            setattr(self, field_name, field.deserialize(data))
+        return data
 
     def make_request(self):
         """Return the response data of a remote endpoint."""
